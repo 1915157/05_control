fca6a325f4083ed48f6f3e5514d61533298e8a37
1915157 <chonsa0409@naver.com>
2019-10-08 20:03:36 +0900

    real practice 01-07

--- main.c
+++ main.c
@@ -1,10 +1,121 @@
-#include <stdio.h>
-#include <stdlib.h>
+// op05_01
+int main(int argc, char *argv[]) {
+	int input;
+	
+	printf("input a number : ");
+	scanf("%d", &input);
+	
+	if (input > 0)
+	{	
+	printf("positive\n");
+	}	
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	else
+	{ 
+	printf("non-positive\n");
+	}
+		
+	return 0;
+	
+
+
+// op05_02
+
+	int input;
+	
+	printf("input a number : ");
+	scanf("%d", &input);
+	
+	if (input > 0)
+	{	
+	printf("|x| = %d", input);
+	}	
+
+	else
+	{ 
+	printf("|x| = %d ", -input);
+	}
+		
+	return 0;
+	
+
+
+
+
+// op05_03
+
+	int input;
+	printf("input a number : ");
+	scanf("%d", &input);
+	
+	if (input > 0)
+	{	
+	printf("positive\n");
+	}	
+		
+	else if (input < 0)
+	{
+		printf("negative\n");
+	}
+	
+	else
+	{ 
+	printf("zero\n");
+	}
+		
+	return 0;
+	
+
+
+
+
+// op05_04
+
+	int i=0;
+	int meter;
+	
+	while(i<3){
+		meter = i * 1609;
+		printf("%d mile is %d meter\n", i, meter);
+		i++;
+	}
+	return 0;
+
+
+
+// op05_05
+
+	int i;
+	for (i=0; i<10; i++)
+		printf("Hello World!\n");
+
+
 
 // op05_06
-int main(int argc, char *argv[]) {
+
+	
+	char c;
+	int num = 0;
+	
+	printf("input a string : ");
+	
+	while( (c= getchar()) != '\n')
+	{
+		if (c<= '9'&& c>= '0')
+		{
+			num++;
+		}
+	
+	}
+	
+	printf("the number of digits is : %i\n", num);
+	return 0;
+
+			
+
+
+// op05_07
+
 	
 	int answer = 50;
 	int input;
@@ -23,19 +134,12 @@ int main(int argc, char *argv[]) {
 		 {
 		 	printf("Low!\n");
 		 }
-		cnt++;   // thinkting about this 
+		cnt++;  
 	}
 	
 	while(input != answer);
 	printf("congratulation! trial : %i\n", cnt);
 	return 0;
 	
-	
 }
-				
-		
-
 	
-	
-
-
